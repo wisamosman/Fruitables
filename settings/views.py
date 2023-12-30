278b26799b61e4baba3245bc7d3fbe3f4ad7f766
wisamosman <viesam_1987@hotmail.com>
2023-12-30 23:21:05 +0100

    add settings views.py

--- settings/views.py
+++ settings/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Company
+from fruit.models import Fruit , Review
 
+
+
+
+def home(request):
+    fruits = Fruit.objects.all()[:8]
+    reviews = Review.objects.all()
+    return render(request,'settings/home.html',{
+        'fruit':fruits,
+        'reviews':reviews,
+    })
+
+# Create your views here.
+
+class CompanyList(generic.ListView):
+    model = Company
 # Create your views here.
